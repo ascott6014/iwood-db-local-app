16fd240e2ead45373c4e0d3eaee69abcbbe8817d
Adam Scott <iamadamone@gmail.com>
2025-07-23 12:19:32 -0500

    added style and print & review functions to customer-form

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded( {extended: true}));
 app.use(express.static('public', {extensions: ['html']}));
+app.use(express.json());
+
 
 // Endpointss
 app.post('/createCustomer', handleCreateNewCustomer); 
